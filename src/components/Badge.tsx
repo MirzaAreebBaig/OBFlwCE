0066fdd9ff4795c3310cfb932ddbbcbe4142948f
MirzaAreebBaig <areeb@deltaisoftware.com>
2023-12-31 11:06:05 +0530

    Brn

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -53,14 +53,14 @@ export const Badge = (props: Props) => {
       Powered by
       <a
         ref={liteBadge}
-        href={'https://flowiseai.com'}
+        href={'https://outsurface.com/onebrain'}
         target="_blank"
         rel="noopener noreferrer"
         class="lite-badge"
         id="lite-badge"
         style={{ 'font-weight': 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span> Flowise</span>
+        <span> OneBrain</span>
       </a>
     </span>
   );
